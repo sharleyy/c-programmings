fc64b29334e6cb345736543fb8c7e7ef1a423676
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-01-26 13:48:53 +0300

    Add files via upload

---
+++ product of two numbers .c
@@ -0,0 +1,16 @@
+/*product of two numbers*/
+#include<stdio.h>
+int main()
+{
+  int a,b,product;
+  printf("Enter value of a:");
+  scanf("%d",&a);
+
+  printf("Enter value of b:");
+  scanf("%d",&b);
+
+  product=a*b;
+  printf("product=%d",product);
+
+  return 0;
+}
