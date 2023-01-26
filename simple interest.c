ccf21ed0cd764565745d796a25c915cd77c3700e
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-01-26 15:40:42 +0300

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,18 @@
+//program to calculate simple interest
+#include<stdio.h>
+int main()
+{
+  float t,r,p,interest;
+  printf("\nEnter the principal amount:");
+scanf("%f",&p);
+
+  printf("\nEnter time:");
+  scanf("%f",&t);
+
+  printf("\nEnter rate:");
+  scanf("%f",&r);
+
+ interest=p*t*r;
+  printf("interest=%f",interest);
+  return 0;
+}
