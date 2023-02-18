33225377d59d57c083f4e08ed3da46d171f161aa
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-02-18 14:30:43 +0300

    Create Divisibility of two.c

---
+++ Divisibility of two.c
@@ -0,0 +1,16 @@
+//Divisibility of two
+#include<stdio.h>
+int main(){
+
+int a;
+printf("Enter the value of a:");
+scanf("%d",&a);
+
+if(a%2==0){
+printf("a is divisible by 2");}
+
+else{
+printf("a is not divisible by 2");}
+
+return 0;
+}
