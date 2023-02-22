f2ca7bcba81c5ee12aee357779dbe2fb3c2ac658
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-02-22 23:44:34 +0300

    Create first 50 natural numbers in reverse order.c

---
+++ first 50 natural numbers in reverse order.c
@@ -0,0 +1,13 @@
+//Program to find the first 50 natural numbers in reverse order
+#include<stdio.h>
+int main(){
+
+int i;
+printf("\nEnter the first 50 natural numbers inreverse order: ");
+  
+for(i = 50;i >= 1;i--){
+printf("%d\n",i);
+} 
+  
+return 0;
+}
