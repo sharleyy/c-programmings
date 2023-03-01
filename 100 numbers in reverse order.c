a99c8ed8e905c008c867981dbe551e6d42f12cbc
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-03-02 00:12:41 +0300

    Create 100 numbers in reverse order.c

---
+++ 100 numbers in reverse order.c
@@ -0,0 +1,13 @@
+//program of the first 100 numbers in reverse order
+#include<stdio.h>
+int main(){
+
+int n = 100;
+while (n>0){
+printf("%d",n);
+n = n-1;
+}
+
+printf("loop body finished");
+return 0;
+}
