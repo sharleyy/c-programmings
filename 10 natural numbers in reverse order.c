5b05c3dacd04c19cff82227305329659438318fa
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-03-02 00:22:01 +0300

    Create 10 natural numbers in reverse order.c

---
+++ 10 natural numbers in reverse order.c
@@ -0,0 +1,12 @@
+//Program to find the first 10 natural numbers in reverse order
+#include<stdio.h>
+int main(){
+
+int i;
+for(i=9;i>=0;i--){
+printf("%d",i);
+}
+
+printf("For loop body finished");
+return 0;
+}
