a6b3837b05f69b002f6053e9373f9c37a17822d5
sharleyy <123085530+sharleyy@users.noreply.github.com>
2023-03-09 15:00:34 +0300

    Create an array of 10 elements.c

---
+++ an array of 10 elements.c
@@ -0,0 +1,12 @@
+//An array of 10 marks
+#include<stdio.h>
+int main()
+{
+  int i,marks[10]={10,29,36,41,50,68,34,56,68,20};
+  for(i=0;i<10;i++)
+
+    {
+      printf("%d ",marks[i]);
+    }
+  return 0;
+}
